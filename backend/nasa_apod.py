13d30fdf2dfd001a3037fb726d0d6845c9881cf1
Kajtek <58279498+Kay-Jar@users.noreply.github.com>
2025-10-09 00:34:11 -0500

    Fixed nasa_apod.py by removing unused imports causing ruff check to fail

--- backend/nasa_apod.py
+++ backend/nasa_apod.py
@@ -1,19 +1,24 @@
-import os, time, json, requests
+import os
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv(), override=False) #For loading the .env file and accessing any sensitive info like API keys
+load_dotenv(
+    find_dotenv(), override=False
+)  # For loading the .env file and accessing any sensitive info like API keys
 
 """
 Return a NASA_API_KEY. If you have a valid .env key in your file, then you will return that key.
 Otherwise, yo uwill return NASA's default "DEMO_KEY" to still use methods.
 """
+
+
 def getNASA_APIKey():
     key = os.getenv("NASA_API_KEY")
-    
-    if (key):
+
+    if key:
         return key
     else:
         return "DEMO_KEY"
+
 
 def getDate():
 
